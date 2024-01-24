4adbb449d916e71290952a7da3a30626563c947c
Yuchang He <rhe@Yuchangs-MacBook-Air.local>
2024-01-23 22:51:02 -0800

    day6

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -5,12 +5,11 @@
 def removeElement(self, nums: List[int], val: int) -> int:
     slow, fast = 0, 0 # slow: track != val and return it, fast keep whole list
     while fast < len(nums):
-        if nums[fast] == val:
-            fast += 1
-            continue # find next one 
-        else:
+        if nums[fast] != val:
             nums[slow] = nums[fast]
             slow += 1
-            fast += 1
+        fast += 1
     return slow
+    
+
     
