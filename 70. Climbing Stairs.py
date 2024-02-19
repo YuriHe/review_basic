1c11e58d01f9bb919b02a207c55f573109b1f83f
Yuchang He <rhe@Yuchangs-MacBook-Air.local>
2024-02-18 20:31:28 -0800

    day32

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,16 @@
+"""
+    Question: how many ways to climb to the top, choice is 1 step or 2steps
+    Topic: DP Memorization
+"""
+def climbStairs(self, n: int) -> int:
+    # create dp array with n+1 size, since need n steps/result
+    dp = [0] * (n+1)
+    # basic case
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    dp[1], dp[2] = 1, 2
+    for i in range(3, len(dp)):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
