4fd63066dd4ea3b73729c089a2c3eccbd0365cf8
Yuchang He <rhe@Yuchangs-MacBook-Air.local>
2024-03-04 22:37:07 -0800

    day48

--- 605. Can Place Flowers.py
+++ 605. Can Place Flowers.py
@@ -2,10 +2,13 @@
     Question: Can place flower avoid neighbor [i-1] and [i+1],
     try to place flowers as much we can
     Topic: Greedy
+    Issue: Boundary
     """
 def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
     for i in range(len(flowerbed)):
+        # check boundary
         if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+            # update flowerbed list
             flowerbed[i] = 1
             n -= 1
         if n <= 0:
