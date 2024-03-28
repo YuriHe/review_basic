4034882c6eeeb51ffd7d522f72877cd25ae33527
Yuchang He <rhe@Yuchangs-MacBook-Air.local>
2024-03-27 23:03:19 -0700

    day69

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -6,12 +6,16 @@
 def isValid(self, s: str) -> bool:
     stack = []
     for c in s:
-        if c == "(" or c == "{" or c == "[":
+        if c == "(" or c == "[" or c == "{":
             stack.append(c)
-        else:
-            if len(stack) == 0: return False
-            if (c == ")" and stack[-1] == "(") or (c == "]" and stack[-1] == "[") or (c == "}" and stack[-1] == "{"):
-            # remove last element from stack list
+        else: # it is close bracket
+            if len(stack) <= 0:
+                return False
+
+            # False scenario will be boarder
+            if (c == "]" and stack[-1] == "[") or (c == "}" and stack[-1] == "{") or (c == ")" and stack[-1] == "("):
                 stack.pop()
-            else: return False
+            else:
+                return False # no match bracket
+        
     return len(stack) == 0
