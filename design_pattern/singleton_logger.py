4b048d029c8b89afd398f10c1864627de2c345b0
yuchangrachel <yheimbusi@gmail.com>
2025-01-21 12:31:55 -0800

    add sort

--- design_pattern/singleton_logger.py
+++
@@ -1,5 +0,0 @@
-class SingletonLogger:
-    _instance = None
-
-    def __init__(self):
-        raise Runn
--- stubs/test/2024-12-1.py
+++
@@ -1,6 +0,0 @@
-"""
-Question: Get shortest compressed representation: AA-> 2A
-1.Removal of K consecutive characters from every possible position in the string.
-2.Recompressing the resulting string and calculating its compressed length.
-3.Returning the minimum compressed length.
-"""
