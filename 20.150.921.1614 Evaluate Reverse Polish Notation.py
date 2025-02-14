e0498db947d1f742f10309096f471c1124e2dfe3
yuchangrachel <yheimbusi@gmail.com>
2025-02-14 14:51:50 -0800

    {

--- 20.150.921.1614 Evaluate Reverse Polish Notation.py
+++ 20.150.921.1614 Evaluate Reverse Polish Notation.py
@@ -7,18 +7,19 @@
 def isValid(self, s: str) -> bool:
     stack = []
     for c in s:
-        if c == "(" or c == "[" or c == "{":
+        if c == "(" or c =="[" or c =="{":
             stack.append(c)
-        else: # it is close bracket
-            if len(stack) <= 0:
-                return False
-
-            # False scenario will be boarder
-            if (c == "]" and stack[-1] == "[") or (c == "}" and stack[-1] == "{") or (c == ")" and stack[-1] == "("):
+        elif not stack:
+            return False
+        else:
+            if c == ')' and stack[-1] == "(":
+                stack.pop()
+            elif c == ']' and stack[-1] == "[":
+                stack.pop()
+            elif c == '}' and stack[-1] == "{":
                 stack.pop()
             else:
-                return False # no match bracket
-        
+                return False
     return len(stack) == 0
 
 
